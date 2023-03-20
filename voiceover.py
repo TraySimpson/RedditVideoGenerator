c8a576505123b35fa4e28881b02c356b3e8922ec
kariuki-kithinji <121386121+kariuki-kithinji@users.noreply.github.com>
2023-03-20 13:21:52 +0300

    Added more realistic voice-over engine
    changed the voice over from pyttsx3 to Mozilla's TTS which is more realistic

--- voiceover.py
+++ voiceover.py
@@ -1,3 +1,4 @@
+"""
 import pyttsx3
 
 voiceoverDir = "Voiceovers"
@@ -5,6 +6,23 @@ voiceoverDir = "Voiceovers"
 def create_voice_over(fileName, text):
     filePath = f"{voiceoverDir}/{fileName}.mp3"
     engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('rate', 125)
+    engine.setProperty('voice', voices[1].id)
     engine.save_to_file(text, filePath)
     engine.runAndWait()
     return filePath
+"""
+
+
+from TTS.api import TTS
+
+voiceoverDir = "Voiceovers"
+
+tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True, gpu=False)
+
+def create_voice_over(fileName, text):
+    filePath = f"{voiceoverDir}/{fileName}.mp3"
+    tts.tts_to_file(text=text,file_path=filePath)
+    return filePath
+
